5b28b999d51731fde72b7c7301b7c1f992d3e1c5
m <devmatheus@gmail.com>
2023-05-20 19:10:15 -0300

    ajustes deploy

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
-
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    
     <div className="container">
       <h1 className="title">tudoparacarro.com.br</h1>
       <p className="description">Este domínio está disponível!</p>
-      <p className="contact-email">Faça sua oferta enviando um e-mail para: <a href="mailto:contato@tudoparacarro.com.br">contato@tudoparacarro.com.br</a></p>
+      <p className="contact-email">
+        Faça sua oferta enviando um e-mail para: 
+        <a href="mailto:contato@tudoparacarro.com.br">
+          contato@tudoparacarro.com.br
+        </a>
+      </p>
     </div>
   );
 }
