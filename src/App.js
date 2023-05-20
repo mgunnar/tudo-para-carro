e685ce635ced1cd56b8e43eece6fab43c7cf431b
m <devmatheus@gmail.com>
2023-05-20 12:13:38 -0300

    Adds react page.

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="title">tudoparacarro.com.br</h1>
+      <p className="description">Este domínio único está disponível para venda!</p>
+      <p className="convincing-text">Se você é um entusiasta de carros ou tem um negócio automotivo, este domínio pode ser seu.</p>
+      <p className="convincing-text">Aproveite esta oportunidade para estabelecer sua presença online e atrair um público apaixonado por carros.</p>
+      <p className="contact-info">Entre em contato agora mesmo e compre com segurança:</p>
+      <p className="contact-email">Envie: <a href="mailto:devmatheus@gmail.com">devmatheus@gmail.com</a></p>
+    </div>
+  );
+}
+
+export default App;
