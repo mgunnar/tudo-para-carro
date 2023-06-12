d8294b157578d47de8c25d9a8453b7b7fa67a147
m <devmatheus@gmail.com>
2023-06-11 21:50:53 -0300

    teste alteracoes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
+import imagemDominio from "./imagem-tpc.png";
 import "./App.css";
 
 function App() {
   return (
-    <div className="container">
-      <h1 className="title">tudoparacarro.com.br</h1>
-      <p className="description">Este domínio está disponível!</p>
-      <p className="contact-email">Faça sua oferta enviando um e-mail para: </p>
-      <p>
-        <a href="mailto:contato@tudoparacarro.com.br">
+    <div className="App">
+        <h1 className="title">tudoparacarro.com.br</h1>
+        <p>Este domínio único está disponível para venda!</p>
+        <p className="convincing-text">Entre em contato agora mesmo,</p>
+        <p className="contact-email">
+          envie um e-mail para:{" "}
+          <a href="mailto:contato@tudoparacarro.com.br">
           contato@tudoparacarro.com.br
         </a>
-      </p>
+        </p>
+        <img className="imagem" src={imagemDominio} />
     </div>
   );
 }
